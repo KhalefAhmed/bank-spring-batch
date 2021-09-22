ca3ad0056940ecee3399359bd2397fb6de054244
Ahmed Khalil Khalef <46876471+a2khalil@users.noreply.github.com>
2021-09-22 21:57:55 +0200

    annotation missing

--- src/main/java/com/example/bankspringbatch/JobRestController.java
+++ src/main/java/com/example/bankspringbatch/JobRestController.java
@@ -4,16 +4,18 @@ import org.springframework.batch.core.*;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@RestController
 public class JobRestController {
 
     @Autowired private JobLauncher jobLauncher;
     @Autowired private Job job;
 
-    @GetMapping
+    @GetMapping("start-job")
     public BatchStatus load() throws Exception {
         Map<String, JobParameter> params = new HashMap<>();
         params.put("time",new JobParameter(System.currentTimeMillis()));
