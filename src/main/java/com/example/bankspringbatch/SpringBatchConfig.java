f0f0ac8442aceb7367065edf39396a6131d569df
Ahmed Khalil Khalef <46876471+a2khalil@users.noreply.github.com>
2021-09-22 20:11:13 +0200

    Job Configuration

--- src/main/java/com/example/bankspringbatch/SpringBatchConfig.java
+++ src/main/java/com/example/bankspringbatch/SpringBatchConfig.java
@@ -1,10 +1,45 @@
 package com.example.bankspringbatch;
 
+import com.example.bankspringbatch.model.BankTransaction;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableBatchProcessing
 public class SpringBatchConfig {
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+    @Autowired
+    private ItemReader<BankTransaction> bankTransactionItemReader;
+    @Autowired
+    private ItemWriter<BankTransaction> bankTransactionItemWriter;
+    @Autowired
+    private ItemProcessor<BankTransaction,BankTransaction> itemProcessor;
+
+    public Job bankJob() {
+        Step step = stepBuilderFactory.get("ETL-Transaction-File-Load")
+                .<BankTransaction,BankTransaction>chunk(100)
+                .reader(bankTransactionItemReader)
+                .writer(bankTransactionItemWriter)
+                .processor(itemProcessor)
+                .build();
+
+        return jobBuilderFactory.get("ETL-Load")
+                .incrementer(new RunIdIncrementer())
+                .start(step)
+                .build();
+    }
+
 
 }
